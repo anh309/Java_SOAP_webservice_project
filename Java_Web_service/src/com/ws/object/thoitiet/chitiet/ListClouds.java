643cdd7f4813d842a27a94397047a973d382fc84
Phạm Xuân Anh <phamxuananh.309@gmail.com>
2018-12-29 14:06:04 +0700

    Create ListClouds.java

---
+++ Java_Web_service/src/com/ws/object/thoitiet/chitiet/ListClouds.java
@@ -0,0 +1,24 @@
+package com.ws.object.thoitiet.chitiet;
+
+public class ListClouds {
+	float all; // tình trạng mây %
+
+	public float getAll() {
+		return all;
+	}
+
+	public void setAll(float all) {
+		this.all = all;
+	}
+
+	public ListClouds(float all) {
+		super();
+		this.all = all;
+	}
+
+	public ListClouds() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
